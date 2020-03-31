c859916f4f5a516fe84c4b52f8edbb21ab68217b
wangxiang <xiangwang04@hand-china.com>
2020-03-31 15:30:49 +0800

    [FIX]增加类型选择

--- src/main/java/io/choerodon/notify/infra/mapper/SendSettingMapper.java
+++ src/main/java/io/choerodon/notify/infra/mapper/SendSettingMapper.java
@@ -34,5 +34,6 @@ public interface SendSettingMapper extends Mapper<SendSettingDTO> {
     List<SendSettingDetailDTO> queryByLevelAndAllowConfig(@Param("level") String level,
                                                           @Param("allowConfig") boolean allowConfig);
 
-    List<SendSettingDTO> pageSendSettingByCondition(@Param("sendSettingDTO") SendSettingDTO sendSettingDTO);
+    List<SendSettingDTO> pageSendSettingByCondition(@Param("sendSettingDTO") SendSettingDTO sendSettingDTO,
+                                                    @Param("type") String type);
 }
