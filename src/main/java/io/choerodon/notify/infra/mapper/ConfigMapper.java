8cb28c432e081c8d35560ef6d49210bfc57bd8dd
isaac <273070111@qq.com>
2020-09-25 16:12:54 +0800

    [FIX]delete redundant back_end codes

--- src/main/java/io/choerodon/notify/infra/mapper/ConfigMapper.java
+++
@@ -1,8 +0,0 @@
-package io.choerodon.notify.infra.mapper;
-
-import io.choerodon.mybatis.common.Mapper;
-import io.choerodon.notify.infra.dto.Config;
-
-public interface ConfigMapper extends Mapper<Config> {
-
-}
--- src/main/java/io/choerodon/notify/infra/mapper/TemplateMapper.java
+++
@@ -1,7 +0,0 @@
-package io.choerodon.notify.infra.mapper;
-
-import io.choerodon.mybatis.common.Mapper;
-import io.choerodon.notify.infra.dto.Template;
-
-public interface TemplateMapper extends Mapper<Template> {
-}
