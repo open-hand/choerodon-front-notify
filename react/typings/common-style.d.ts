d01ada83092ace8364afa2499950f61429f2e6ae
812913 <lwfvirgo@163.com>
2020-10-22 15:54:29 +0800

    [IMP] 删除通知服务页面规则相关代码

--- react/typings/common-style.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '@choerodon/agile/lib/routes/Issue/components/BatchModal/useFields';
-declare module '@choerodon/agile-pro/lib/components/select/select-demand-type';
-declare module '@choerodon/agile-pro/lib/components/select/select-demand-classification';
-declare module '@choerodon/agile-pro/lib/components/select/select-priority';
-declare module '@choerodon/agile/lib/utils/common';
-declare module '@choerodon/agile/lib/components/Loading';
