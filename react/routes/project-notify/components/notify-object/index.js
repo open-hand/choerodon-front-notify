30b7c683d8b461ca6c772008e6ba46dd16762b68
20615 <wenfei.li@hand-china.com>
2021-03-09 20:49:26 +0800

    [IMP] 更改冲刺延期通知对象设置

--- react/routes/project-notify/components/notify-object/index.js
+++ react/routes/project-notify/components/notify-object/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { includes } from 'lodash';
 import { Form, SelectBox, Select } from 'choerodon-ui/pro';
 import { useProjectNotifyStore } from '../../stores';
 
@@ -6,19 +7,18 @@ import './index.less';
 
 const { Option } = Select;
 
-export default ({ record, allSendRoleList }) => {
+export default ({ record, allSendRoleList, excludesRole = [] }) => {
   const {
     intlPrefix,
     prefixCls,
     intl: { formatMessage },
   } = useProjectNotifyStore();
 
-
   return (
     <div className={`${prefixCls}-object-content`}>
       <Form record={record}>
         <SelectBox name="sendRoleList" vertical>
-          {allSendRoleList.map((item) => (
+          {allSendRoleList.filter((item) => !includes(excludesRole, item)).map((item) => (
             <Option value={item} key={item}>
               <span className={`${prefixCls}-object-content-checkbox`}>
                 {formatMessage({ id: `${intlPrefix}.object.${item}` })}
