5dd14374d435872d5dae753fc25e3490d70b5b4e
isaac <273070111@qq.com>
2021-05-24 17:12:01 +0800

    [FIX]初步修改按钮点击不loading的问题

--- react/routes/NotifySetting/mail-setting/MailSettingForm.jsx
+++ react/routes/NotifySetting/mail-setting/MailSettingForm.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
-import { Form, TextField, Select, SelectBox, Password, NumberField } from 'choerodon-ui/pro';
+import {
+  Form, TextField, Select, SelectBox, Password, NumberField,
+} from 'choerodon-ui/pro';
 import '../index.less';
 
 const { Option } = Select;
@@ -16,6 +18,6 @@ export default (props) => (
       <Option value>是</Option>
       <Option value={false}>否</Option>
     </SelectBox>
-    <NumberField name="port" />
+    <NumberField numberGrouping={false} name="port" />
   </Form>
 );
